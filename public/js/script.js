cd9db4f8920abaddc18a4d76706dcf9e69dfbdd9
kokkabanjar <kokkabanjar@gmail.com>
2023-05-06 13:45:41 +0800

    versi 01,  save script

--- public/js/script.js
+++ public/js/script.js
@@ -37,6 +37,7 @@ function mapAlifaBjb() {
 function gabunganMaps() {
     return mapAlifaBjb(), mapAlifaMartapura();
 }
-// let script = document.createElement('script');
-// script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBjeNAV-4FNLg5jmwAtvflzb74lfb2LuAM&callback=gabunganMaps';
-// document.head.appendChild(script);
+
+let script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBjeNAV-4FNLg5jmwAtvflzb74lfb2LuAM&callback=gabunganMaps';
+document.head.appendChild(script);
